e3c570a2297aecc5d336d0e77110ed0a35b12158
SergeyRO <sergey_r.o@mail.ru>
2022-08-18 22:00:32 +0300

    Stack modify

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-from queue import LifoQueue
-
-
-class Stack(LifoQueue):
-    # def __init__(self):
-    #     ...
+class Stack:
+    def __init__(self):
+        self.stack = []
 
     def isEmpty(self):
         #True/False пустой или нет
-        return self.empty()
+        if len(self.stack) > 0:
+            return False
+        else:
+            return True
 
     def push(self, a):
         #add Element
-        self.put(a)
+        return self.stack.append(a)
 
     def pop(self):
         #drop Element возвращает верхний элемент
-        return self.get()
+        return self.stack.pop()
 
     def peek(self):
         #возвращает верхний элемент стека. Стек не меняется
-
-        return self.get()
+        if not self.isEmpty():
+            return self.stack[len(self.stack)-1]
+        else:
+            return None
 
     def size(self):
         #возвращает количество элементов
-        return self.qsize()
+        return len(self.stack)
+
+    def balance(self, parentheses):
+        self.stack.clear()
+        if len(parentheses)%2 != 0:
+            return "Несбалансированно"
+        else:
+            for item in parentheses:
+                if stack.peek() == '{' and item == '}':
+                    stack.pop()
+                elif stack.peek() == '[' and item == ']':
+                    stack.pop()
+                elif stack.peek() == '(' and item == ')':
+                    stack.pop()
+                else:
+                    stack.push(item)
+            if stack.isEmpty():
+                return "Сбалансированны"
+            else:
+                return "Несбалансированны"
 
 if __name__ == '__main__':
+    #Для задания №1
     stack = Stack()
     print(stack.isEmpty())
     stack.push(10)
@@ -36,4 +58,7 @@ if __name__ == '__main__':
     print(stack.isEmpty())
     print(stack.size())
     print(stack.pop())
-    print(stack.size())
+    print(stack.peek())
+
+    #Для задания №2
+    print(stack.balance('[{[{(()])}}]'))
